faa2f0732a3804c41d6f18071dc78e9301df5c02
Tetri Mesquita <tetri.mesquita@gmail.com>
2020-04-16 11:30:04 -0300

    atualização de dados

--- assets/js/search.js
+++ assets/js/search.js
@@ -1,48 +1,16 @@
 (function () {
   function displaySearchResults(results, store) {
-    var searchResults = document.getElementById("search-results");
+    let searchResults = document.getElementById("search-results");
 
     if (results.length) {
       // Are there any results?
-      var appendString = "";
+      let appendString = "";
 
-      for (var i = 0; i < results.length; i++) {
+      for (let i = 0; i < results.length; i++) {
         // Iterate over the results
-        var item = store[results[i].ref];
+        let item = store[results[i].ref];
 
-        appendString += '<div class="card border-secondary mb-3">'
-        + '<div class="card-body">'
-        + '    <h5 class="card-title">' + item.nome + '</h5>';
-
-        if (item.produtos != '')
-        appendString += '    <p class="card-text">' + item.produtos + '</p>';
-
-        appendString += '</div>'
-        + '<ul class="list-group list-group-flush">';
-        
-        if (item.local != '')
-        appendString += '    <li class="list-group-item">' + item.local + ' <a'
-        + '           href="https://www.google.com/maps/search/' + item.local + '" rel="noreferrer" title="' + item.local + '"'
-        + '            target="_blank" aria-label="ver no mapa"><i class="fas fa-map-marker-alt"></i></a></li>';
-
-        if (item.telefone != '')
-        appendString += '    <li class="list-group-item text-right">' + item.telefone + '</li>';
-        
-        appendString += '</ul>';
-        appendString += '<div class="card-body">';
-
-        if (item.link != '') {
-          if (item.link.startsWith('http')) {
-            appendString += '    <a class="card-link" href="' + item.link + '" rel="noreferrer" title="' + item.nome + '" target="_blank">' + item.link + '</a>';
-          } else if (item.link.includes('www')) {
-            appendString += '    <a class="card-link" href="http://' + item.link + '" rel="noreferrer" title="' + item.nome + '" target="_blank">' + item.link + '</a>';
-          }
-        }
-        else
-        appendString += '    ' + item.link;
-
-        appendString += '</div>';
-        appendString += '</div>';
+        appendString += montaCard(item);
       }
 
       searchResults.innerHTML = appendString;
@@ -52,11 +20,11 @@
   }
 
   function getQueryVariable(variable) {
-    var query = window.location.search.substring(1);
-    var vars = query.split("&");
+    let query = window.location.search.substring(1);
+    let vars = query.split("&");
 
-    for (var i = 0; i < vars.length; i++) {
-      var pair = vars[i].split("=");
+    for (let i = 0; i < vars.length; i++) {
+      let pair = vars[i].split("=");
 
       if (pair[0] === variable) {
         return decodeURIComponent(pair[1].replace(/\+/g, "%20"));
@@ -64,14 +32,14 @@
     }
   }
 
-  var searchTerm = getQueryVariable("query");
+  let searchTerm = getQueryVariable("query");
 
   if (searchTerm) {
     document.getElementById("search-box").setAttribute("value", searchTerm);
 
     // Initalize lunr with the fields it will be searching on. I've given title
     // a boost of 10 to indicate matches on this field are more important.
-    var idx = lunr(function () {
+    let idx = lunr(function () {
       this.field("id");
       this.field("data");
       this.field("nome", { boost: 10 });
@@ -83,7 +51,7 @@
       this.field("ciente");
     });
 
-    for (var key in window.store) {
+    for (let key in window.store) {
       // Add the data to lunr
       idx.add({
         id: key,
@@ -97,7 +65,7 @@
         ciente: window.store[key].ciente
       });
 
-      var results = idx.search(searchTerm); // Get lunr to perform a search
+      let results = idx.search(searchTerm); // Get lunr to perform a search
       displaySearchResults(results, window.store); // We'll write this in the next section
     }
   }
--- default.js
+++ default.js
@@ -8,3 +8,63 @@ window.addEventListener("load", () => {
     }
   }
 });
+
+function montaCard(item) {
+  let card =
+    '<div class="card border-secondary mb-3">' +
+    '<div class="card-body">' +
+    '    <h5 class="card-title">' +
+    item.nome +
+    "</h5>";
+
+  if (item.produtos != "")
+    card += '    <p class="card-text">' + item.produtos + "</p>";
+
+  card += "</div>" + '<ul class="list-group list-group-flush">';
+
+  if (item.local != "")
+    card +=
+      '    <li class="list-group-item">' +
+      item.local +
+      " <a" +
+      '           href="https://www.google.com/maps/search/' +
+      item.local +
+      '" rel="noreferrer" title="' +
+      item.local +
+      '"' +
+      '            target="_blank" aria-label="ver no mapa"><i class="fas fa-map-marker-alt"></i></a></li>';
+
+  if (item.telefone != "")
+    card +=
+      '    <li class="list-group-item text-right">' + item.telefone + "</li>";
+
+  card += "</ul>";
+  card += '<div class="card-body">';
+
+  if (item.link != "") {
+    if (item.link.startsWith("http")) {
+      card +=
+        '    <a class="card-link" href="' +
+        item.link +
+        '" rel="noreferrer" title="' +
+        item.nome +
+        '" target="_blank">' +
+        item.link +
+        "</a>";
+    } else if (item.link.includes("www")) {
+      card +=
+        '    <a class="card-link" href="http://' +
+        item.link +
+        '" rel="noreferrer" title="' +
+        item.nome +
+        '" target="_blank">' +
+        item.link +
+        "</a>";
+    }
+  } else card += "    " + item.link;
+
+  card += "</div>";
+  card += "</div>";
+
+  return card;
+}
