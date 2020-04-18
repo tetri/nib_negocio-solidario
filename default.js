a187b6a0f81b6f56dd654fdb975234cbd33ca9b4
Tetri Mesquita <tetri.mesquita@gmail.com>
2020-04-18 09:19:37 -0300

    atualização de dados

--- default.js
+++ default.js
@@ -30,19 +30,53 @@ function montaCard(item) {
   if (item.link) {
     card += '<div class="card-body">';
 
-    if (item.link.startsWith("http")) {
-      card += `    <a class="card-link" href="${item.link}" rel="noreferrer" title="${item.nome}" target="_blank">${item.link}</a>`;
-    } else if (item.link.startsWith("www")) {
-      card += `    <a class="card-link" href="http://'${item.link}" rel="noreferrer" title="${item.nome}" target="_blank">${item.link}</a>`;
-    } else {
-      card += item.link;
+    let link = [];
+    for (let str of item.link.split(' ')) {
+      if (!str)
+        continue;
+
+      //console.debug('str in link', link)
+      if (validURL(str)) {
+        if (str.startsWith('www'))
+          link.push(`<a class="card-link" href="http://${str}" rel="noreferrer" title="${item.nome}" target="_blank">${str}</a>`);
+        else
+          link.push(`<a class="card-link" href="${str}" rel="noreferrer" title="${item.nome}" target="_blank">${str}</a>`);
+        continue;
+      } 
+
+      if (validEmail(str)) {
+        link.push(`<a class="card-link" href="mailto:${str}" rel="noreferrer" title="${item.nome}">${str}</a>`);
+        continue;
+      }
+
+      if (str.startsWith('@')) 
+        link.push(`<a class="card-link" href="https://instagram.com/${str.replace('@', '')}" rel="noreferrer" title="${item.nome}" target="_blank">${str}</a>`);
+      else
+        link.push(str);
     }
+    card += link.join(' ');
+
     card += '</div>';
   }
 
   card += "</div>";
 
   return card;
+}
+
+function validURL(str) {
+  var pattern = new RegExp('^(https?:\\/\\/)?'+ // protocol
+    '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|'+ // domain name
+    '((\\d{1,3}\\.){3}\\d{1,3}))'+ // OR ip (v4) address
+    '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*'+ // port and path
+    '(\\?[;&a-z\\d%_.~+=-]*)?'+ // query string
+    '(\\#[-a-z\\d_]*)?$','i'); // fragment locator
+  return !!pattern.test(str);
+}
+
+function validEmail(email) {
+  var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(String(email).toLowerCase());
 }
 
 // -- search.js
