f820f52dd060af4fb9b6cdeddb7fcde45dbc852a
Tetri Mesquita <tetri.mesquita@gmail.com>
2020-05-22 08:30:04 -0300

    atualização de dados

--- default.js
+++ default.js
@@ -249,6 +249,7 @@ for (let item of items) {
 categorias = ordenarDicionarioPorChave(categorias);
 displayCategories(categorias);
 
+items = shuffle(items);
 let _items = items.slice();
 paginas = chunkArr(items, 9);
 //console.debug('paginas', paginas);
@@ -360,3 +361,22 @@ $(document).on("click", ".categoria", function () {
   filtrarPorCategoria($(this).data("categoria"));
   return false;
 });
+
+function shuffle(array) {
+  var currentIndex = array.length, temporaryValue, randomIndex;
+
+  // While there remain elements to shuffle...
+  while (0 !== currentIndex) {
+
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+
+    // And swap it with the current element.
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
+
+  return array;
+}
