0126b52e59195ed30765c1dedea1702d18f60d28
Tetri Mesquita <tetri.mesquita@gmail.com>
2020-07-28 07:48:49 -0300

    atualização de dados

--- functions/sqlquery.py
+++ functions/sqlquery.py
@@ -2,7 +2,7 @@ import os
 import sqlite3
 import pandas as pd
 
-if (not os.path.isfile('data.db')):
+if (not os.path.isfile('_data/negocio-solidario.db')):
     #data_url = 'https://docs.google.com/spreadsheets/d/1if5WstOy_zJTdLdR2rlfxuuVSsQEh0EZVQtSylsSmwY/export?format=csv'
     data_url = '_data/negocio-solidario.csv'
 
@@ -11,28 +11,21 @@ if (not os.path.isfile('data.db')):
 
     nib = pd.read_csv(data_url, header=None, names=headers)
 
-    with sqlite3.connect('negocio-solidario.db') as conn:
+    with sqlite3.connect('_data/negocio-solidario.db') as conn:
         nib.to_sql('nib', conn, dtype={
-            'id': 'integer',
             'data': 'date',
             'categoria': 'text',
             'nome': 'text',
             'produtos': 'text',
             'local': 'text',
             'telefone': 'text',
-            'telefone2': 'text',
-            'telefone3': 'text',
-            'telefone4': 'text',
             'entrega': 'text',
             'link': 'text',
-            'link2': 'text',
-            'link3': 'text',
-            'link4': 'text',
             'ciente': 'text'
         })
 
 def sql_query(query):
-    with sqlite3.connect('data.db') as conn:
+    with sqlite3.connect('_data/negocio-solidario.db') as conn:
         cur = conn.cursor()
         cur.execute(query)
         rows = cur.fetchall()
@@ -40,14 +33,14 @@ def sql_query(query):
 
 
 def sql_edit(query, var):
-    with sqlite3.connect('data.db') as conn:
+    with sqlite3.connect('_data/negocio-solidario.db') as conn:
         cur = conn.cursor()
         cur.execute(query, var)
         conn.commit()
 
 
 def sql_delete(query, var):
-    with sqlite3.connect('data.db') as conn:
+    with sqlite3.connect('_data/negocio-solidario.db') as conn:
         cur = conn.cursor()
         cur.execute(query, var)
         conn.commit()
